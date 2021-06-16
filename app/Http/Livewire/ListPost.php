e7714da8b91c66aa21ce05fbc0e6e91c6c574cfe
rzhasibuan <rzhasibuan@gmail.com>
2021-06-17 01:01:06 +0700

    add edit

--- app/Http/Livewire/ListPost.php
+++ app/Http/Livewire/ListPost.php
@@ -11,10 +11,30 @@ class ListPost extends Component
         "postCreated" => '$refresh',
     ];
 
+    public $updateSteteId = 0;
+    public $body = '';
+
     public function render()
     {
         return view('livewire.list-post',[
             'posts' => Post::latest()->get(),
         ]);
+    }
+
+    public function showUpdateForm($postId)
+    {
+        $this->updateSteteId = $postId;
+        $post = Post::find($postId);
+        $this->body = $post->body;
+    }
+
+    public function updatePost($postId)
+    {
+        $post = Post::find($postId);
+        $post->body = $this->body;
+        $post->save();
+
+        // berfungsi agar state datanya tertutup kembali kerna bernilai angka 0
+        $this->updateSteteId = 0;
     }
 }
--- resources/views/livewire/list-post.blade.php
+++ resources/views/livewire/list-post.blade.php
@@ -5,9 +5,29 @@
             <div>
                 <span class="text-xl font-bold">{{ $post->user->name }}</span>
                 <span class="text-gray-500">{{ $post->created_at->diffForHumans() }}</span>
+                <button 
+                    wire:click="showUpdateForm({{ $post->id }})"
+                    class="bg-gray-500 p-2 text-white rounded-sm hover:bg-gray-400">
+                    Edit
+                </button>
             </div>
             <div>
+                @if ($updateSteteId !== $post->id)
                 <span>{{ $post->body }}</span>
+                @endif
+
+                @if ($updateSteteId === $post->id)
+                <textarea 
+                wire:model="body" 
+                rows="3" 
+                class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" 
+                ></textarea>
+                <button
+                wire:click="updatePost({{ $post->id }})"
+                class="px-4 py-2 m-1 bg-blue-600 text-white hover:bg-blue-500 rounded-md">
+                    save
+                </button>
+                @endif
             </div>
         </div>
     @endforeach
