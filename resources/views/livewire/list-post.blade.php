6a09f022252d2a5d4bfe0b5fe22bcc181d978fd6
rzhasibuan <rzhasibuan@gmail.com>
2021-06-23 00:27:04 +0700

    buttom

--- resources/views/livewire/list-post.blade.php
+++ resources/views/livewire/list-post.blade.php
@@ -8,16 +8,20 @@
 
                     <span class="text-xl font-bold">{{ $post->user->name }}</span>
                     <span class="text-gray-500 px-1">{{ $post->created_at->diffForHumans() }}</span>
-                    {{-- <button 
+                    <button 
                         wire:click="showUpdateForm({{ $post->id }})"
-                        class="bg-gray-500 p-2 mx-2 text-white rounded-lg hover:bg-gray-400 hover:shadow-lg transform-all duration-300">
-                        Edit
+                        class=" p-2 mx-2 rounded-lg transform-all duration-300 hover:shadow-lg ring-2 ring-gray-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                        </svg>
                     </button>
                     <button
                     wire:click="deletePost({{ $post->id }})" 
-                    class="bg-red-500 p-2 text-white hover:bg-red-400 rounded-lg hover:bg-gray-400 hover:shadow-lg transform-all duration-300">
-                        Delete
-                    </button> --}}
+                    class="p-2 hover:text-red-400 rounded-lg transform-all duration-300 hover:shadow-xl ring-2 ring-red-300 ">
+                       <svg xmlns="http://www.w3.org/2000/svg" class="text-red-500 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
                 </div>
             <a href="">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
